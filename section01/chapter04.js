76be437a421a5aad0a36897a2379a58b364eb3ca
minsung <minsung21c@gmail.com>
2024-12-26 22:54:25 +0900

    자바스크립트 함수표현식, 콜백함수 테스팅

---
+++ section01/chapter04.js
@@ -0,0 +1,14 @@
+//1. 함수선언식, 함수표현식, 화살표 함수
+
+function getArea(width, height) {
+  //중첩함수
+  function anotherFunction() {
+    console.log("another function 입니다.");
+  }
+  anotherFunction();
+
+  return width * height;
+}
+
+let area1 = getArea(10, 50);
+console.log(area1);
