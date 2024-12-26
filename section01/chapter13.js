71b1b4f88b40b806547b964ea20c1b945e0e79c2
minsung <minsung21c@gmail.com>
2024-12-26 22:56:55 +0900

    자바스크립트 구조분해할당 테스팅

---
+++ section01/chapter13.js
@@ -0,0 +1,25 @@
+// 1. 배열의 구조 분해 할당
+let arr = [1, 2, 3];
+let [one, two, three, four] = arr;
+//console.log(one, two, three, four);
+
+// 2. 객체의 구조 분해 할당
+let person = {
+  name: "홍길동",
+  age: 27,
+  hobby: "테니스",
+};
+
+let {
+  age, //age 변수명을 myAge
+  hobby,
+  name,
+  extra = "hello",
+} = person;
+//console.log(age, name, hobby, extra);
+
+//3. 객체 구조 분해 할당을 이용해서 함수 매개변수 받는 방법
+const func = ({ name, age, hobby }) => {
+  console.log(name, age, hobby);
+};
+func(person);
