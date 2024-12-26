8fd5ba05cc181621571a2066104e40b87b200368
minsung <minsung21c@gmail.com>
2024-12-26 22:58:53 +0900

    자바스크립트 얕은복사, 깊은복사 테스팅

---
+++ section01/chapter15.js
@@ -0,0 +1,27 @@
+//얕은 객체복사
+const person = {
+  name: "kdj",
+  age: 29,
+  tall: 178,
+};
+
+let person2 = person;
+person2.tall = 180;
+//console.log(person);
+//console.log(person2);
+
+//깊은 객체복사
+let person3 = { ...person };
+person3.tall = 180;
+//console.log(person);
+//console.log(person3);
+
+//객체비교(주소끼리 비교할까? 아니다. 값으로 비교한다.)
+console.log(person);
+console.log(person2);
+console.log(person3);
+
+console.log(person === person2);
+console.log(person === person3);
+console.log(person2 === person3);
+console.log(JSON.stringify(person2) === JSON.stringify(person3));
