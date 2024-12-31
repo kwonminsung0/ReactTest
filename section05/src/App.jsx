ceb75424cc82f9dbe78303876bbb36dbf732e3dc
minsung <minsung21c@gmail.com>
2024-12-31 22:18:23 +0900

    React 회원가입 기능구현, 카운터앱 기능 구현, React hooks 기능 테스팅

---
+++ section05/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HookExam from "./components/HookExam";
+import Register from "./components/Register";
+
+function App() {
+  console.log("App");
+  return (
+    <>
+      <HookExam />
+    </>
+  );
+}
+
+export default App;
