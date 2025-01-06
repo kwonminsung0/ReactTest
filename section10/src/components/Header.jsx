79b2334cac0fde96e8170684c77b3144fbf475f2
minsung <minsung21c@gmail.com>
2025-01-06 17:38:10 +0900

    오늘의 할일 체크리스트 프로그램 체크카운트 기능 추가

---
+++ section10/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+import { memo } from "react";
+
+const Header = () => {
+  console.log("Header");
+  return (
+    <div className="header">
+      <h3>오늘은 📅</h3>
+      <h1>{new Date().toDateString()}</h1>
+    </div>
+  );
+};
+
+export default memo(Header);
